d24c13614778c3b009610a04cb32ded62ec2b49a
Oleh Smolynets <smolynets@gmail.com>
2016-12-15 23:07:10 +0200

    different directories for templates and views

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -59,16 +59,25 @@ WSGI_APPLICATION = 'studentsdb.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
+    # }
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
-    
-    'ENGINE': 'django.db.backends.mysql',
-    'HOST': 'localhost',
-    'USER': 'admin',
-    'PASSWORD': '0000',
-    'NAME': 'school',
-  }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb1',
+        'USER': 'oleh1',
+        'PASSWORD': '0000',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+    #'default': {
+      #'ENGINE': 'django.db.backends.mysql',
+      #'HOST': 'localhost',
+      #'USER': 'students_db_user',
+      #'PASSWORD': '0000',
+      #'NAME': 'school',
+    #}
 }
 
 # Internationalization
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -3,28 +3,28 @@ from django.contrib import admin
 from .settings import MEDIA_ROOT, DEBUG
 
 urlpatterns = patterns('',
-    url(r'^$', 'students.views.podii', name='podii'),
-    url(r'^biblioteka/$', 'students.views.biblioteka', name='biblioteka'),
-    url(r'^vuhovna/$', 'students.views.vyhovna', name='vyhovna'),
-    url(r'^muzey/$', 'students.views.muzey', name='muzey'),
-    url(r'^kolek/$', 'students.views.kolek', name='kolek'),
-    url(r'^psuholoh/$', 'students.views.psuholoh', name='psuholoh'),
-    url(r'^zno/$', 'students.views.zno', name='zno'),
-    url(r'^rozklad/$', 'students.views.rozklad', name='rozlad'),
-    url(r'^istoria/$', 'students.views.istoria', name='istoria'),
+    url(r'^$', 'students.view.podii.podii', name='podii'),
+    url(r'^biblioteka/$', 'students.view.biblioteka.biblioteka', name='biblioteka'),
+    url(r'^vuhovna/$', 'students.view.vyhovna.vyhovna', name='vyhovna'),
+    url(r'^muzey/$', 'students.view.muzey.muzey', name='muzey'),
+    url(r'^kolek/$', 'students.view.kolek.kolek', name='kolek'),
+    url(r'^psuholoh/$', 'students.view.psuholoh.psuholoh', name='psuholoh'),
+    url(r'^zno/$', 'students.view.zno.zno', name='zno'),
+    url(r'^rozklad/$', 'students.view.rozklad.rozklad', name='rozlad'),
+    url(r'^istoria/$', 'students.view.istoria.istoria', name='istoria'),
     ###adding
-    url(r'^podii_add/$', 'students.views.podii_add', name='podii_add'),
+    url(r'^podii_add/$', 'students.view.podii.podii_add', name='podii_add'),
 
 
 
     ###edit
     url(r'^podii/(?P<pk>\d+)/edit/$',
-       'students.views.podii_edit', name='podii_edit'),
+       'students.view.podii.podii_edit', name='podii_edit'),
 
 
     ###delete
     url(r'^podii/(?P<pk>\d+)/delete/$',
-       'students.views.podii_delete', name='podii_delete'),
+       'students.view.podii.podii_delete', name='podii_delete'),
 
 
     url(r'^admin/', include(admin.site.urls)),
